671e874dc5846b62a2530055e5e2c32f35e55df0
AymenBenaissa <37246135+AymenBenaissa@users.noreply.github.com>
2020-10-29 16:47:06 +0100

    Question 1.8.2 et 1.8.3 : Fait

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,7 +1,10 @@
 package com.opstty;
 
 import com.opstty.job.WordCount;
+import district.job.DistrictCount;
 import org.apache.hadoop.util.ProgramDriver;
+import species.job.ExistingSpecies;
+import treesSpecies.job.TreesSpecies;
 
 public class AppDriver {
     public static void main(String argv[]) {
@@ -11,6 +14,12 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("districtcount", DistrictCount.class,
+                    "A map/reduce program that counts the number of district in the input files.");
+            programDriver.addClass("existingspecies", ExistingSpecies.class,
+                    "A map/reduce program that display all the existing species in the input files.");
+            programDriver.addClass("treesspecies", TreesSpecies.class,
+                    "A map/reduce program that display all the number of trees by species in the input files.");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
